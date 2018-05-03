7079e66a9f06ec9d1dea0ec8bfc4be7594360727
Taylor Gentry <taylor.gentry300@gmail.com>
2018-05-03 16:42:22 -0400

    commit number 2

--- main.js
+++ main.js
@@ -1,49 +1,86 @@
-const userInput = prompt("Rock, Paper, or Scissors?")
+const container = document.getElementById("container")
+
+const userInput = prompt("Rock, Paper, or Scissors?").toLowerCase()
+
 
 function addPhoto(photo) {
-    
+
 }
 
-function makediv(content) {
-    div = document.createElement("div")
-    div.textContent = content
-    document.getElementById("container").appendChild(div)
+function attachImage(choice) {
+    const imageContainer = document.createElement("div")
+    imageContainer.classList.add("image-container")
+
+    const heading = document.createElement("h3")
+    heading.classList.add("image-heading")
+    heading.textContent = choice
+    imageContainer.appendChild(heading)
+
+    const image = document.createElement("img")
+    image.src = choice + ".jpg"
+    image.classList.add("choice-image", choice)
+    imageContainer.appendChild(image)
+
+    container.appendChild(imageContainer)
 }
-let computerChoice = Math.random()
-if (computerChoice < 0.34) {
+
+function makediv(node) {
+    const div = document.createElement("div")
+    div.classList.add("element-wrapper")
+    div.appendChild(node)
+    container.appendChild(div)
+}
+
+const chance = Math.random()
+let computerChoice
+if (chance < 0.34) {
     computerChoice = "rock"
-} else if (computerChoice <= 0.67) {
+} else if (chance <= 0.67) {
     computerChoice = "paper"
 } else {
     computerChoice = "scissors"
 }
-let compare = function (choice1, choice2) {
-    if (choice1 === choice2) {
+
+
+
+let compare = function (userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
         return "It's a tie!!!"
     }
-    if (choice1 === "rock") {
-        if (choice2 === "scissors"){
-            return "You Win!"
-    } else {
-        return "You Lose!"
-    }
-}
-    if (choice1 === "scissors") {
-       if (choice2 === "paper") {
+    if (userChoice === "rock") {
+        if (computerChoice === "scissors") {
             return "You Win!"
         } else {
             return "You Lose!"
         }
     }
-    if (choice1 === "paper") {
-            if (choice2 === "rock") {
-                return "You Win!"
-            } else {
-                return "You Lose!"
-            }
+    if (userChoice === "scissors") {
+        if (computerChoice === "paper") {
+            return "You Win!"
+        } else {
+            return "You Lose!"
         }
     }
+    if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            return "You Win!"
+        } else {
+            return "You Lose!"
+        }
+    }
+}
 
 
 
-makediv(compare(userInput, computerChoice))
+makediv(document.createTextNode(compare(userInput, computerChoice)))
+
+function putPhotosOnPage() {
+    if (userInput === computerChoice) {
+        attachImage("tie")
+    } else {
+        attachImage(userInput)
+        attachImage(computerChoice)
+    }
+}
+
+putPhotosOnPage()
